ec2266cdbdfec754958f547ee03a3292a9d1defd
RyanSAMAMA <ryan.samama@ensae.fr>
2026-02-07 03:12:09 +0100

    start of the project, intialisation with uv, qdrant tests

---
+++ main.py
@@ -0,0 +1,36 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+endpoint_qdrant = os.getenv("QDRANT_ENDPOINT")
+api_qdrant = os.getenv("QDRANT_API")
+
+
+qdrant_client = QdrantClient(
+    url=endpoint_qdrant,
+    api_key=api_qdrant,
+)
+
+qdrant_client.create_collection(
+    collection_name="test",
+    vectors_config=VectorParams(size=4, distance=Distance.COSINE),
+)
+
+operation_info = qdrant_client.upsert(
+    collection_name="test",
+    wait=True,
+    points=[
+        PointStruct(id=1, vector=[0.05, 0.61, 0.76, 0.74], payload={"city": "Berlin"}),
+        PointStruct(id=2, vector=[0.19, 0.81, 0.75, 0.11], payload={"city": "London"}),
+        PointStruct(id=3, vector=[0.36, 0.55, 0.47, 0.94], payload={"city": "Moscow"}),
+        PointStruct(id=4, vector=[0.18, 0.01, 0.85, 0.80], payload={"city": "New York"}),
+        PointStruct(id=5, vector=[0.24, 0.18, 0.22, 0.44], payload={"city": "Beijing"}),
+        PointStruct(id=6, vector=[0.35, 0.08, 0.11, 0.44], payload={"city": "Mumbai"}),
+    ],
+)
+
+print(operation_info)
